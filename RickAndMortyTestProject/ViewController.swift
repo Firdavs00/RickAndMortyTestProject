3cc12efda2d69e920e28bfaef18b8d7c8887fa4e
Firdavs00 <firdavsvlog@gmail.com>
2022-11-19 18:40:11 +0500

    Initial Commit

---
+++ RickAndMortyTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyTestProject
+//
+//  Created by Firdavs Boltayev on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
